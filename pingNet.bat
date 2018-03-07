9574087a5aaf56f3cf7b47954d1cf7d7802b8ab2
Matt Hannan <matthannan1@gmail.com>
2018-03-07 14:28:15 -0500

    Updated pingNet.bat
    Pings the broadcast address

--- pingNet.bat
+++ pingNet.bat
@@ -2,30 +2,35 @@
 SETLOCAL ENABLEDELAYEDEXPANSION
 rem Execute from CMD line as such: "pingNet.bat 10.25.68"
 rem Where 10 25 and 68 are the network octets in 10.25.68.0.
-rem 
-rem
-rem Delete files if they exists
-rem
+
+
+:: Delete files if they exists
+set "switch=%1"
 IF EXIST pingScanResults.txt del /F pingScanResults.txt
 IF EXIST cleanedPingScanResults.txt del /F cleanedPingScanResults.txt
- 
-rem
-rem
-rem Find the live addresses
-rem
+
+goto :comment12
+
+:: Ping all addresses
 echo %1 
 for /l %%i in (1,1,254) do @(
-        echo Pinging: %1.%%i
-        ping %1.%%i -n 1 -w 100 | find /i "Reply">>pingScanResults.txt
-        )
-rem
-rem
-rem Clean up the ScanResults.txt file
-rem
-rem
+	echo Pinging: %1
+	ping %1 -n 1 -w 100 | find /i "Reply">>pingScanResults.txt
+	)
+	
+:comment12
+
+ping %1.255 
+
+
+goto :comment13
+	
+::Clean up the ScanResults.txt file
 for /f "tokens=3 delims= " %%a in (pingScanResults.txt) do (
-        set S=%%a
-        set S=!S:~0,-1!
-        >> cleanedPingScanResults.txt echo.!S!
-        )
+	set S=%%a
+	set S=!S:~0,-1!
+	>> cleanedPingScanResults.txt echo.!S!
+	)
+	
 del /F pingScanResults.txt
+:comment13
